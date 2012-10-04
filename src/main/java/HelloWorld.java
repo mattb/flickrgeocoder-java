5ce6d72c9ce1b992be2b11a59527c8e7af5353c7
Matt Biddulph <mb@hackdiary.com>
2012-10-04 21:09:51 +0100

    Flickr shapes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,16 @@
+import com.vividsolutions.jts.*;
+import com.vividsolutions.jts.index.*;
+import com.vividsolutions.jts.index.strtree.*;
+import com.vividsolutions.jts.geom.*;
+import java.io.*;
+import java.util.*;
+import org.geotools.feature.simple.*;
+import org.geotools.feature.simple.*;
+import org.geotools.data.*;
+import org.geotools.data.shapefile.*;
+import org.opengis.feature.simple.*;
+import org.geotools.geometry.jts.JTS;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -5,20 +18,30 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
 public class HelloWorld extends HttpServlet {
-
+  public STRtree tree = new STRtree();
+  public HelloWorld() throws IOException {
+    org.geotools.util.logging.Logging.GEOTOOLS.setLoggerFactory(org.geotools.util.logging.Log4JLoggerFactory.getInstance());
+    org.apache.log4j.LogManager.getLogger("org.geotools").setLevel(org.apache.log4j.Level.OFF);
+    ShapefileDataStore data = new ShapefileDataStore(getClass().getResource("flickr_shapes_public_dataset_2.0/flickr_shapes_localities/OGRGeoJSON.shp"));
+    int count = 0;
+    for(FeatureReader<SimpleFeatureType, SimpleFeature> reader = data.getFeatureReader() ; reader.hasNext(); count++) {
+      SimpleFeature f = reader.next();
+      tree.insert(JTS.toGeometry(f.getBounds()).getEnvelopeInternal(),f);
+    }
+  }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
+    throws ServletException, IOException {
+    resp.getWriter().print(tree.size() + " shapes loaded.");
     }
 
-    public static void main(String[] args) throws Exception{
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
-        server.start();
-        server.join();   
-    }
+  public static void main(String[] args) throws Exception{
+    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+    server.start();
+    server.join();   
+  }
 }
