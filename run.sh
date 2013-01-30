d97737aab484ca07e050c12507cf83e097292891
straup <straup@gmail.com>
2013-01-30 14:16:23 +0100

    tweak CLI so that the first argument is the port number the server should listen on

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-PORT=5000
+PORT=0
+SHAPES=''
+
+for param in "$@"
+do
+
+    if test $PORT = 0
+    then
+	PORT=$param
+    else
+	SHAPES="${SHAPES} ${param}"
+    fi
+done
+
 export PORT
 
 JAVA=`which java`
 
-${JAVA} -Xmx384m -cp 'target/classes:target/dependency/*' com.hackdiary.geo.FlickrGeocodeServlet $*
+${JAVA} -Xmx500m -cp 'target/classes:target/dependency/*' com.hackdiary.geo.FlickrGeocodeServlet ${SHAPES}
